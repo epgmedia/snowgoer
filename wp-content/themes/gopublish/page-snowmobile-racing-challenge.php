93243cf94ab6a297bc70849aca88fef325abe766
Christopher Gerber <chriswgerber@gmail.com>
2014-11-25 20:58:30 -0600

    Quick changes to snowgoer racing page.

--- wp-content/themes/gopublish/page-snowmobile-racing-challenge.php
+++ wp-content/themes/gopublish/page-snowmobile-racing-challenge.php
@@ -45,27 +45,27 @@ get_header();
         <div style="float: left; width: 495px;">
 
             <hr />
-
             <h3>The Rules:</h3>
-
             <p>
-	            The <em>Snow Goer</em> Snowmobile Racing Challenge game is back for the 2014-15
-	            racing season. It’s easy to understand, easy to play and it is free – the only
-	            thing it costs you is a few minutes to make your predictions, and you can win
-	            prizes and bragging rights.
+	            The <em>Snow Goer</em> Snowmobile Racing Challenge game presented by Amsoil is
+	            back for the 2014-15 racing season. It’s easy to understand, easy to play and
+	            it is free – the only thing it costs you is a few minutes to make your
+	            predictions, and you can win prizes and bragging rights.
             </p>
 	        <p>
-	            The plan is to again have several different rounds of competition, based on
-	            various forms of snowmobile racing – Snocross, Cross-Country and Ovals.
-	            Separate points will be kept in each form of racing, and there will be a
-	            champion for each – play one, or play them all.
+		        The game will again have several different rounds of competition, based on
+		        various forms of snowmobile racing – Snocross, Cross-Country and Ovals.
+		        Separate points will be kept in each form of racing, and there will be a
+		        champion for each – play one, or play them all. New presenting sponsor Amsoil
+		        is promising prizes for segment winners, to include Amsoil Interceptor oil
+		        and Amsoil swap.
 	        </p>
-	        <p>
+			<p>
 	            For each race, players will select the top six finishing order, based on a
 	            pull-down window that lists the names of the racers who race in that are
 	            expected to compete in the chosen race. Players will select a winner as well
 	            as each of the next five finishing positions.
-	        </p>
+			</p>
 	        <p>
 	            If a player correctly selects the position of a racer (whether correctly
 	            selecting a given racer to finish first, third or sixth, for example) he or
@@ -73,7 +73,6 @@ get_header();
 	            predicted finishing position, the player will earn a varying amount of points
 	            based on this formula:
             </p>
-
             <ul>
                 <li>Driver in correct position = 20 points</li>
                 <li>Driver finishes 1 position off of the prediction = 15 points</li>
@@ -83,13 +82,11 @@ get_header();
                 <li>Driver finishes 5 positions off = 11 points</li>
                 <li>Driver finishes 6 or more positions off = 0 points</li>
             </ul>
-
             <p>
 	            Players only receive points for racers who finish in the Top 6 at the event,
 	            and once a player’s picks for a given round are made, they are final; the
 	            player cannot go back in and change them.
             </p>
-
             <hr />
 
         </div>
@@ -97,21 +94,16 @@ get_header();
         <div class="racingchallengeraces" style="float: right; width: 400px;">
 
             <h3>Race Schedule:</h3>
-
             <p>
                 Here are the planned races.
             </p>
-
             <p>
                 For snocross, the focus will be the Pro Open class.
             </p>
-
             <p>
                 For oval races, it will be Pro Champ 440.
             </p>
-
             <h4>Snocross</h4>
-
             <ul>
                 <li>Nov. 30, Duluth, Minnesota</li>
 	            <li>Dec. 13, Fargo, North Dakota</li>
@@ -123,22 +115,25 @@ get_header();
 	            <li>Feb. 28, Mt. Pleasant, Michigan</li>
 	            <li>March 15, Lake Geneva, Wisconsin</li>
             </ul>
-
             <h4>Ovals</h4>
-
             <ul>
                 <li>TBD</li>
             </ul>
 
         </div>
 
+	    <div style="width:410px;float:right;">
+		    <img src="http://www.snowgoer.com/wp-content/uploads/2014/11/SG-LOGO-e1416970489982.jpg" width="410" style="margin:auto;display:block;" />
+		    <img src="http://www.snowgoer.com/wp-content/uploads/2014/11/AMSOIL_RGB_wTag-e1416970477302.jpg" width="410" style="margin:auto;display:block;" />
+	    </div>
+
         <div style="clear: both">
 
             <p>
 	            <em>Snow Goer</em> will recognize weekly winners as well as champions for the
-                nine snocross races combined, the various oval/enduro races combined and an
-                overall high points season champion. There will be occasional prize packages
-                available during the season, depending on program sponsorship.
+				nine snocross races combined, the various oval/enduro races combined and an
+				overall high points season champion. There will be occasional prize packages
+				available during the season, depending on program sponsorship.
             </p>
 
             <p>
